7f83a4e0ecf3693e799d4f5f9139427883f6a010
Enzin030 <eeeenzin@gmail.com>
2025-09-11 17:46:48 +0800

    CLB

--- map.js
+++ map.js
@@ -11,156 +11,128 @@
 
     /* 創建圖層按鈕 */
     function createLayerButton(map, baseLayers, themeLayers, thumbnails, options = {}) {
-
-        const mainClusterGroup = L.markerClusterGroup({
-            iconCreateFunction: cluster => {
-                const count = cluster.getChildCount();
-                const a = [0.7, 0.8, 0.9][+(count > 10) + (count > 20)];
-                const color = `rgba(2, 125, 180, ${a})`;
-                const size = Math.min(40 + count * 2, 60);
-                return L.divIcon({
+        const cluster = options.sharedClusterGroup || L.markerClusterGroup({
+            iconCreateFunction: c => {
+            const count = c.getChildCount();
+            const a = [0.7, 0.8, 0.9][+(count > 10) + (count > 20)];
+            const color = `rgba(2, 125, 180, ${a})`;
+            const size = Math.min(40 + count * 2, 60);
+            return L.divIcon({
                 html: `<div style="background:${color};border-radius:50%;width:${size}px;height:${size}px;display:flex;align-items:center;justify-content:center;color:#fff;font-size:14px">${count}</div>`,
                 className: 'marker-cluster'
-                });
+            });
             }
         });
-        map.addLayer(mainClusterGroup);
+        if (!options.sharedClusterGroup) map.addLayer(cluster);
 
-        const {
-            showLayerButton = true,
-            showZoomControl = true
-        } = options;
+        const LayerButton = L.Control.extend({
+            options: { position: options.position || 'topright' },
+            onAdd: function () {
+            const container = L.DomUtil.create('div', 'layer-button-container');
+            const button = L.DomUtil.create('button', 'layer-button', container);
+            button.innerHTML = '<svg xmlns="https://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24"><path fill="#fff" d="m13.387 3.425l6.365 4.243a1 1 0 0 1 0 1.664l-6.365 4.244a2.5 2.5 0 0 1-2.774 0L4.248 9.332a1 1 0 0 1 0-1.664l6.365-4.243a2.5 2.5 0 0 1 2.774 0m6.639 8.767a2 2 0 0 1-.577.598l-6.05 4.084a2.5 2.5 0 0 1-2.798 0l-6.05-4.084a2 2 0 0 1-.779-2.29l6.841 4.56a2.5 2.5 0 0 0 2.613.098l.16-.098l6.841-4.56a2 2 0 0 1-.201 1.692m0 3.25a2 2 0 0 1-.577.598l-6.05 4.084a2.5 2.5 0 0 1-2.798 0l-6.05-4.084a2 2 0 0 1-.779-2.29l6.841 4.56a2.5 2.5 0 0 0 2.613.098l.16-.098l6.841-4.56a2 2 0 0 1-.201 1.692"/></svg>&nbsp;圖層';
 
-        if (showLayerButton) {
-            const LayerButton = L.Control.extend({
-                onAdd: function (map) {
-                var container = L.DomUtil.create('div', 'layer-button-container');
-                L.DomEvent.disableClickPropagation(container);
-                L.DomEvent.disableScrollPropagation(container);
+            const popup = L.DomUtil.create('div', 'layer-popup', container);
+            popup.style.display = 'none';
 
-                var button = L.DomUtil.create('button', 'layer-button', container);
-                button.innerHTML = '<svg xmlns="https://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24"><path fill="#fff" d="m13.387 3.425l6.365 4.243a1 1 0 0 1 0 1.664l-6.365 4.244a2.5 2.5 0 0 1-2.774 0L4.248 9.332a1 1 0 0 1 0-1.664l6.365-4.243a2.5 2.5 0 0 1 2.774 0m6.639 8.767a2 2 0 0 1-.577.598l-6.05 4.084a2.5 2.5 0 0 1-2.798 0l-6.05-4.084a2 2 0 0 1-.779-2.29l6.841 4.56a2.5 2.5 0 0 0 2.613.098l.16-.098l6.841-4.56a2 2 0 0 1-.201 1.692m0 3.25a2 2 0 0 1-.577.598l-6.05 4.084a2.5 2.5 0 0 1-2.798 0l-6.05-4.084a2 2 0 0 1-.779-2.29l6.841 4.56a2.5 2.5 0 0 0 2.613.098l.16-.098l6.841-4.56a2 2 0 0 1-.201 1.692"/></svg>&nbsp;圖層';
-
-                var popup = L.DomUtil.create('div', 'layer-popup', container);
-                popup.style.display = 'none';
-
-                var popupContent = `
-                    <div class="popup-content">
+            function render() {
+                let html = `
+                <div class="popup-content">
                     <div class="popup-title">底圖</div>
                     <div class="layer-options-container">`;
-                Object.keys(baseLayers).forEach(function (layerName) {
-                    var content = thumbnails[layerName];
-                    let thumbHtml = '';
-                    if (typeof content === 'string' && (content.startsWith('http') || content.startsWith('data:image'))) {
-                    thumbHtml = `<img src="${content}" alt="${layerName}" class="thumbnail-img">`;
-                    } else {
-                    thumbHtml = `<div class="thumbnail-svg">${content}</div>`;
-                    }
-                    popupContent += `
-                    <div class="layer-option base-layer" data-layer="${layerName}">
-                        <div class="thumbnail-container">${thumbHtml}</div>
-                        <span>${layerName}</span>
+                Object.keys(baseLayers).forEach(name => {
+                const content = thumbnails[name];
+                const thumb = (typeof content === 'string' && (content.startsWith('http') || content.startsWith('data:image')))
+                    ? `<img src="${content}" alt="${name}" class="thumbnail-img">`
+                    : `<div class="thumbnail-svg">${content}</div>`;
+                const selected = map.hasLayer(baseLayers[name]) ? 'selected' : '';
+                html += `
+                    <div class="layer-option base-layer ${selected}" data-layer="${name}">
+                    <div class="thumbnail-container">${thumb}</div>
+                    <span>${name}</span>
                     </div>`;
                 });
-                popupContent += `
+                html += `
                     </div>
-                    </div>`;
-
-                popupContent += `
-                    <div class="popup-content">
+                </div>
+                <div class="popup-content">
                     <div class="popup-title">主題圖</div>
                     <div class="layer-options-container">`;
-                Object.keys(themeLayers).forEach(function (layerName) {
-                    var content = thumbnails[layerName];
-                    let thumbHtml = '';
-                    if (typeof content === 'string' && (content.startsWith('http') || content.startsWith('data:image'))) {
-                    thumbHtml = `<img src="${content}" alt="${layerName}" class="thumbnail-img">`;
-                    } else {
-                    thumbHtml = `<div class="thumbnail-svg">${content}</div>`;
-                    }
-                    popupContent += `
-                    <div class="layer-option theme-layer" data-layer="${layerName}">
-                        <div class="thumbnail-container">${thumbHtml}</div>
-                        <span>${layerName}</span>
+                Object.keys(themeLayers).forEach(name => {
+                const content = thumbnails[name];
+                const thumb = (typeof content === 'string' && (content.startsWith('http') || content.startsWith('data:image')))
+                    ? `<img src="${content}" alt="${name}" class="thumbnail-img">`
+                    : `<div class="thumbnail-svg">${content}</div>`;
+                const layer = themeLayers[name];
+                const isOn = (layer instanceof L.TileLayer) ? map.hasLayer(layer) : cluster.hasLayer(layer);
+                const selected = isOn ? 'selected' : '';
+                html += `
+                    <div class="layer-option theme-layer ${selected}" data-layer="${name}">
+                    <div class="thumbnail-container">${thumb}</div>
+                    <span>${name}</span>
                     </div>`;
                 });
-                popupContent += `
+                html += `
                     </div>
-                    </div>`;
+                </div>`;
+                popup.innerHTML = html;
 
-                popup.innerHTML = popupContent;
-
-                var firstBaseLayerName = Object.keys(baseLayers)[0];
-                if (firstBaseLayerName) {
-                    baseLayers[firstBaseLayerName].addTo(map);
-                    var selectedBase = popup.querySelector(`.base-layer[data-layer="${firstBaseLayerName}"]`);
-                    if (selectedBase) selectedBase.classList.add('selected');
-                }
-
-                L.DomEvent.on(button, 'click', function (e) {
+                popup.querySelectorAll('.base-layer').forEach(opt => {
+                L.DomEvent.on(opt, 'click', e => {
                     L.DomEvent.stop(e);
-                    popup.style.display = (popup.style.display === 'none') ? 'block' : 'none';
-                });
-
-                popup.querySelectorAll('.base-layer').forEach(function (option) {
-                    L.DomEvent.on(option, 'click', function (e) {
-                    L.DomEvent.stop(e);
-                    const layerName = this.getAttribute('data-layer');
-                    Object.values(baseLayers).forEach(function (layer) {
-                        if (map.hasLayer(layer)) map.removeLayer(layer);
-                    });
-                    baseLayers[layerName].addTo(map);
+                    const name = opt.getAttribute('data-layer');
+                    Object.values(baseLayers).forEach(l => { if (map.hasLayer(l)) map.removeLayer(l); });
+                    baseLayers[name].addTo(map);
                     popup.querySelectorAll('.base-layer').forEach(el => el.classList.remove('selected'));
-                    this.classList.add('selected');
-                    });
+                    opt.classList.add('selected');
+                });
                 });
 
-                popup.querySelectorAll('.theme-layer').forEach(function (option) {
-                    L.DomEvent.on(option, 'click', function (e) {
+                popup.querySelectorAll('.theme-layer').forEach(opt => {
+                L.DomEvent.on(opt, 'click', e => {
                     L.DomEvent.stop(e);
-                    const layerName = this.getAttribute('data-layer');
-                    const layer = themeLayers[layerName];
+                    const name = opt.getAttribute('data-layer');
+                    const layer = themeLayers[name];
                     if (layer instanceof L.TileLayer) {
-                        if (map.hasLayer(layer)) {
+                    if (map.hasLayer(layer)) {
                         map.removeLayer(layer);
-                        this.classList.remove('selected');
-                        } else {
-                        layer.addTo(map);
-                        layer.bringToFront();
-                        this.classList.add('selected');
-                        }
+                        opt.classList.remove('selected');
                     } else {
-                        if (activeClusterThemes.has(layerName)) {
-                        if (typeof layer.getLayers === 'function') {
-                            mainClusterGroup.removeLayers(layer.getLayers());
-                        } else {
-                            mainClusterGroup.removeLayer(layer);
-                        }
-                        activeClusterThemes.delete(layerName);
-                        this.classList.remove('selected');
-                        } else {
-                        if (typeof layer.getLayers === 'function') {
-                            mainClusterGroup.addLayers(layer.getLayers());
-                        } else {
-                            mainClusterGroup.addLayer(layer);
-                        }
-                        activeClusterThemes.add(layerName);
-                        this.classList.add('selected');
-                        }
+                        layer.addTo(map).bringToFront();
+                        opt.classList.add('selected');
                     }
-                    });
+                    } else {
+                    if (cluster.hasLayer(layer)) {
+                        cluster.removeLayer(layer);
+                        opt.classList.remove('selected');
+                    } else {
+                        cluster.addLayer(layer);
+                        opt.classList.add('selected');
+                    }
+                    }
                 });
+                });
+            }
 
-                return container;
-                }
+            if (!Object.keys(baseLayers).some(n => map.hasLayer(baseLayers[n]))) {
+                const first = Object.keys(baseLayers)[0];
+                if (first) baseLayers[first].addTo(map);
+            }
+
+            L.DomEvent.on(button, 'click', e => {
+                L.DomEvent.stop(e);
+                popup.style.display = (popup.style.display === 'none') ? 'block' : 'none';
             });
-            const layerButton = new LayerButton({ position: 'topright' });
-            layerButton.addTo(map);
+            L.DomEvent.disableClickPropagation(container);
+            L.DomEvent.disableScrollPropagation(container);
+
+            render();
+            return container;
+            }
+        });
+
+        return LayerButton;
         }
-        if (showZoomControl) {
-            L.control.zoom({ position: 'topright' }).addTo(map);
-        }
-    }
 
     /* 創建圖標叢集 */
     function createSharedClusterGroup() {
